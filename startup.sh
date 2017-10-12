55bc0671a226c05cf346d6197f80a437e8bf3df2
t-yoneya <t-yoneya@infiniteloop.co.jp>
2017-10-12 10:58:36 +0900

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,40 +1,42 @@
 # 2017-10-07
 # Please start up with zsh
 
-echo Maintan Now
+# echo Maintan Now
 
-# echo Start make vim libraly
+echo Start make vim libraly
 
-# home=$HOME
+home=$HOME
 # .vimディレクトリをカレントに作成
-# cd $home
-# mkdir .vim
-# cd .vim
-# mkdir bundle
-# mkdir colors
+cd $home
+mkdir .vim
+cd .vim
+mkdir bundle
+mkdir colors
 
-# Github="https://github.com"
+Github="https://github.com"
 # vimで使用するライブラリの確保
-# cd bundle
-# git clone "$Github/Shougo/neobundle.vim"
-# git clone "$Github/Shougo/neocomplcache"
-# git clone "$Github/airblade/vim-gitgutter"
-# # git clone "$Github/itchyny/lightline.vim"
-# git clone "$Github/miyakogi/seiya.vim"
-# git clone "$Github/scrooloose/nerdtree"
-# git clone "$Github/tpope/vim-fugitive"
-# cd ../
-# cd colors
-# git clone "$Github/tomasr/molokai"
+cd bundle
+git clone "$Github/Shougo/neobundle.vim"
+git clone "$Github/Shougo/neocomplcache"
+git clone "$Github/airblade/vim-gitgutter"
+git clone "$Github/itchyny/lightline.vim"
+git clone "$Github/miyakogi/seiya.vim"
+git clone "$Github/scrooloose/nerdtree"
+git clone "$Github/tpope/vim-fugitive"
+cd ../
+cd colors
+git clone "$Github/tomasr/molokai"
+mv molokai/colors/molokai.vim molokai.vim
+rm -rf molokai
 
-# echo DONE!!
-# echo Start make Symlinks at HOME
+echo DONE!!
+echo Start make Symlinks at HOME
 
 # シムリンクを作成する
-# cd $HOME/dotfiles
-# ln -sf $HOME/.vimrc _vimrc
-# ln -sf $HOME/.zshrc _zshrc
-# ln -sf $HOME/.tmux.conf _tmux_conf
-# ln -sf $HOME/.zprofile _zprofile
+cd $HOME
+ln -sf $HOME/dotfiles/_vimrc .vimrc
+ln -sf $HOME/dotfiles/_zshrc .zshrc
+ln -sf $HOME/dotfiles/_tmux_conf .tmux.conf
+ln -sf $HOME//dotfiles/_zprofile .zprofile
 
-# echo DONE!!
+echo DONE!!
