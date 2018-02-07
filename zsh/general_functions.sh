61ee52051c98a08eee4ecf456e8c44e4c951e7d9
TakatsuguYoneya <rice410shopper@gmail.com>
2018-02-07 18:45:00 +0900

    add ls function in zsh keybind

--- zsh/general_functions.sh
+++ zsh/general_functions.sh
@@ -2,18 +2,29 @@
 # general functions
 #
 function peco-select-history() {
-  BUFFER="$(\history -n 1 | tac | peco)"
-  CURSOR=$#BUFFER
+  local SELECTED_HISTORY="$(\history -n 1 | tac | peco)"
+  if [ -n "$SELECTED_HISTORY" ]; then
+    BUFFER="$SELECTED_HISTORY"
+    CURSOR=$#BUFFER
+  fi
   zle accept-line
   # zle clear-screen
 }
 zle -N peco-select-history
 bindkey '^r' peco-select-history
 
-# function peco-select-directory() {
-#   BUFFER="cd $(\ls -d */ | peco)"
-#   CURSOR=$#BUFFER
-#   zle accept-line
-# }
-# zle -N peco-select-directory
-# bindkey '' peco-select-directory
+function peco-select-ls() {
+  local SELECTED_LIST="$(\ls | peco)"
+  if [ -n "$SELECTED_LIST" ]; then
+    if [ -n "$LBUFFER" ]; then
+      local new_left="${LBUFFER%\ } $SELECTED_LIST"
+    else
+      local new_left="$SELECTED_LIST"
+    fi
+    BUFFER=${new_left}${RBUFFER}
+    CURSOR=${#new_left}
+  fi
+  zle accept-line
+}
+zle -N peco-select-ls
+bindkey '^l' peco-select-ls
--- zsh/git_functions.sh
+++ zsh/git_functions.sh
@@ -4,7 +4,7 @@
 function peco-git-add() {
   local SELECTED_FILE_TO_ADD="$(git st --porcelain | peco --query "$LBUFFER" | awk -F ' ' '{print $NF}' )"
   if [ -n "$SELECTED_FILE_TO_ADD" ]; then
-    BUFFER="git add $(echo "$SELECTED_FILE_TO_ADD" | tr '\n' ' ')"
+    BUFFER="git add $(echo "$SELECTED_FILE_TO_ADD" | tr '\n' ' ') && git st"
     CURSOR="$#BUFFER"
   fi
   zle accept-line
@@ -16,7 +16,7 @@ bindkey "^ga" peco-git-add
 function peco-git-checkout() {
   local SELECTED_BRANCH="$(git br -a | cut -b 3- | grep -v -- "->" | peco | sed -e "s/\* //g")"
   if [ -n "$SELECTED_BRANCH" ]; then
-    BUFFER="git co $(echo "$SELECTED_BRANCH" | tr '\n' ' ')"
+    BUFFER="git co $(echo "$SELECTED_BRANCH" | tr '\n' ' ') && git st"
     CURSOR="$#BUFFER"
   fi
   zle accept-line
