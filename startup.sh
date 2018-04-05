6d20ca8f416563d7b17b97f72f69acd6b8ebd201
t-yoneya <t-yoneya@infiniteloop.co.jp>
2018-04-05 19:56:54 +0900

    edit startup.sh

--- startup.sh
+++ startup.sh
@@ -7,10 +7,12 @@ home=$HOME
 # .vimディレクトリをカレントに作成
 cd $HOME
 mkdir .vim
+mkdir .zsh
 cd .vim
 mkdir colors
 mkdir dein
 
+# vim library
 Github="https://github.com"
 cd ~/.vim/colors
 git clone "$Github/tomasr/molokai"
@@ -21,16 +23,19 @@ cd ~/.vim/dein
 git clone "$Github/Shougo/dein.vim"
 rm -rf molokai
 rm -rf vim-hybrid
+echo DONE!!
 
+# zsh library
+echo Start make zsh library
+cd ~/.zsh/
+git clone "$Github/b4b4r07/zsh-vimode-visual"
 echo DONE!!
 
 echo Start make tmux library
-
 cd $HOME
 mkdir .tmux
 cd .tmux
 git clone "$Github/tmux-plugin/tmp ~/.tmux/plugins/tpm"
-
 echo DONE!!
 
 echo Start make Symlinks at HOME
