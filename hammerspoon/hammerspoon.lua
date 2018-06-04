208ecbd196251753f6dcd49e57422cb1099d64d4
Takatsugu Yoneya <rice410shopper@gmail.com>
2018-06-04 20:21:24 +0900

    edit hammerspoon.lua

--- hammerspoon/hammerspoon.lua
+++ hammerspoon/hammerspoon.lua
@@ -1,6 +1,10 @@
 -- author: Takatsugu Yoneya
 
-hs.hotkey.bind({"cmd", "alt", "shift"}, "L", function()
+-- -------------------------------------
+--     ** window management **
+-- -------------------------------------
+
+hs.hotkey.bind({"cmd", "alt", "shift"}, "Right", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -13,7 +17,7 @@ hs.hotkey.bind({"cmd", "alt", "shift"}, "L", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "shift"}, "H", function()
+hs.hotkey.bind({"cmd", "alt", "shift"}, "Left", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -23,6 +27,32 @@ hs.hotkey.bind({"cmd", "alt", "shift"}, "H", function()
   f.y = max.y
   f.w = max.w / 2
   f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "shift"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "shift"}, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w
+  f.h = max.h - (max.h / 2)
   win:setFrame(f)
 end)
 
@@ -39,7 +69,32 @@ hs.hotkey.bind({"cmd", "alt", "shift"}, "M", function()
   win:setFrame(f)
 end)
 
+-- -------------------------------------
+--     ** application management **
+-- -------------------------------------
+
+
+-- -------------------------------------
+--        ** debug management **
+-- -------------------------------------
+
+
+
+function reloadConfig(files)
+  local doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+  end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+-- Well, sometimes auto-reload is not working, you know u.u
 hs.hotkey.bind({"cmd", "alt", "shift"}, "R", function()
   hs.reload()
 end)
-hs.alert.show("Config reloaded!!")
+hs.alert.show("Config loaded")
