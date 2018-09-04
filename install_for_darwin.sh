6b79fad696c6dddbbd2f01fdbd6a5b820233bcd8
Takatsugu Yoneya <rice410shopper@gmail.com>
2018-09-04 10:58:08 +0900

    add zplug

--- install_for_darwin.sh
+++ install_for_darwin.sh
@@ -52,9 +52,7 @@ echo DONE!!
 # zsh library
 echo Start make zsh library
 cd ~/.zsh/
-git clone "$Github/b4b4r07/zsh-vimode-visual"
-git clone "$Github/zsh-users/zsh-syntax-highlighting"
-git clone "$Github/zsh-users/zsh-autosuggestions"
+git clone "$Github/zplug/zplug"
 echo DONE!!
 
 echo Start make tmux library
