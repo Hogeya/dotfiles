ff1531653ba8c3e7c962c25cfe606ac25b5cd99d
TakatsuguYoneya <rice410shopper@gmail.com>
2018-09-18 18:28:40 +0900

    delete --reverse

--- zsh/git_functions.sh
+++ zsh/git_functions.sh
@@ -2,7 +2,7 @@
 # git functions
 #
 function fzf-git-add() {
-  local SELECTED_FILE_TO_ADD="$(git st --porcelain | fzf --multi --reverse --query "$LBUFFER" | awk -F ' ' '{print $NF}' )"
+  local SELECTED_FILE_TO_ADD="$(git st --porcelain | fzf --multi --query "$LBUFFER" | awk -F ' ' '{print $NF}' )"
   if [ -n "$SELECTED_FILE_TO_ADD" ]; then
     BUFFER="git add $(echo "$SELECTED_FILE_TO_ADD" | tr '\n' ' ') && git st"
     CURSOR="$#BUFFER"
@@ -13,7 +13,7 @@ zle -N fzf-git-add
 bindkey " ga" fzf-git-add
 
 function fzf-git-reset() {
-  local SELECTED_FILE_TO_ADD="$(git st --porcelain | fzf --multi --reverse --query "$LBUFFER" | awk -F ' ' '{print $NF}' )"
+  local SELECTED_FILE_TO_ADD="$(git st --porcelain | fzf --multi --query "$LBUFFER" | awk -F ' ' '{print $NF}' )"
   if [ -n "$SELECTED_FILE_TO_ADD" ]; then
     BUFFER="git reset $(echo "$SELECTED_FILE_TO_ADD" | tr '\n' ' ') && git st"
     CURSOR="$#BUFFER"
@@ -24,7 +24,7 @@ zle -N fzf-git-reset
 bindkey " gr" fzf-git-reset
 
 function fzf-git-checkout() {
-  local SELECTED_BRANCH="$(git br -a | cut -b 3- | grep -v -- "->" | sed -e "s/remotes\/origin\///g" | fzf --reverse | sed -e "s/\* //g")"
+  local SELECTED_BRANCH="$(git br -a | cut -b 3- | grep -v -- "->" | sed -e "s/remotes\/origin\///g" | fzf | sed -e "s/\* //g")"
   if [ -n "$SELECTED_BRANCH" ]; then
     BUFFER="git co $(echo "$SELECTED_BRANCH" | tr '\n' ' ') && git st"
     CURSOR="$#BUFFER"
@@ -35,7 +35,7 @@ zle -N fzf-git-checkout
 bindkey " gc" fzf-git-checkout
 
 function fzf-git-branch-d() {
-  local SELECTED_BRANCH="$(git br -a | cut -b 3- | grep -v -- "->" | fzf --multi  --reverse | sed -e "s/\* //g")"
+  local SELECTED_BRANCH="$(git br -a | cut -b 3- | grep -v -- "->" | fzf --multi | sed -e "s/\* //g")"
   if [ -n "$SELECTED_BRANCH" ]; then
     BUFFER="git br -D $(echo "$SELECTED_BRANCH" | tr '\n' ' ')"
     CURSOR="$#BUFFER"
