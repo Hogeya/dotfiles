b710cb772c012be71789de4305bfb140a8172722
Takatsugu Yoneya <rice410shopper@gmail.com>
2018-12-26 15:04:34 +0900

    edit hammerspoon

--- hammerspoon/hammerspoon.lua
+++ hammerspoon/hammerspoon.lua
@@ -6,67 +6,15 @@
 
 local mash = {"alt", "cmd"}
 
-hs.hotkey.bind(mash, "L", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind(mash, "H", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind(mash, "K", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h / 2
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind(mash, "J", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y + (max.h / 2)
-  f.w = max.w
-  f.h = max.h - (max.h / 2)
-  win:setFrame(f)
-end)
-
 hs.hotkey.bind(mash, "M", hs.grid.maximizeWindow)
 
 -- multi monitor
-hs.hotkey.bind(mash, "Right", function()
+hs.hotkey.bind(mash, "H", function()
   hs.grid.pushWindowNextScreen()
   hs.grid.maximizeWindow()
 end)
 
-hs.hotkey.bind(mash, "Left", function()
+hs.hotkey.bind(mash, "L", function()
   hs.grid.pushWindowPrevScreen()
   hs.grid.maximizeWindow()
 end)
